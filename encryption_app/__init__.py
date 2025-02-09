19dad49b4d909636532b1afeef93d7806ec4b3e7
Jackbalo <78875978+jackbalo@users.noreply.github.com>
2025-02-09 16:21:15 +0000

    Update __init__.py

--- encryption_app/__init__.py
+++ encryption_app/__init__.py
@@ -18,7 +18,7 @@ csrf = CSRFProtect()
 #migrate = Migrate()
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder="encryption_app/templates", static_folder="encryption_app/static")
     app.config.from_object(Config)
     Session(app)
 
